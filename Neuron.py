5945f165900eacb823fd8448d2e3e5b86ab02116
Glebfra <gleb.frantsuzov@yandex.ru>
2023-03-10 14:22:12 +0300

    + backpropagation method

--- Neuron.py
+++ Neuron.py
@@ -6,11 +6,14 @@ class Neuron(object):
     def __init__(self, weights: np.ndarray, bias: float):
         self.weights = weights.reshape((len(weights), 1))
         self.bias = bias
+        self.inputs: np.ndarray = np.array([])
+
         self.activation_function = activation_function
         self.activation_function_derivative = activation_function_derivative
 
     def feed_forward(self, inputs: np.ndarray):
         inputs = inputs.reshape((1, len(inputs)))
+        self.inputs = inputs
         return float(self.activation_function(inputs @ self.weights + self.bias))
 
     def back_propagation(self):
