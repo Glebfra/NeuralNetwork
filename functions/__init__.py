c8d750c917ea5d362b9c03b27a80f775913174b4
Glebfra <gleb.frantsuzov@yandex.ru>
2023-03-21 01:26:43 +0300

    ! changed the network structure, + added cython

--- functions/__init__.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def activation_function(x):
-    return 1 / (1 + np.exp(-x))
-
-
-def activation_function_derivative(x):
-    return (1-activation_function(x)) * activation_function(x)
